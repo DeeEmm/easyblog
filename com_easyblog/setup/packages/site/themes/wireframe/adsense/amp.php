edb01b82c188d3c253a0a82abf0d5502b7ae7724
DeeEmm <deeemm@deeemm.com>
2025-02-12 00:16:48 +1030

    Initial Fork

---
+++ com_easyblog/setup/packages/site/themes/wireframe/adsense/amp.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @package      EasyBlog
+* @copyright    Copyright (C) Stack Ideas Sdn Bhd. All rights reserved.
+* @license      GNU/GPL, see LICENSE.php
+* EasyBlog is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+defined('_JEXEC') or die('Unauthorized Access');
+?>
+<amp-ad
+	width="100vw"
+	height="320"
+	type="adsense"
+	data-ad-client="<?php echo $data['client']; ?>"
+	data-ad-slot="<?php echo $data['slot']; ?>"
+	data-auto-format="rspv"
+	data-full-width=""
+><div overflow=""></div>
+</amp-ad>
